4e826490dff3c08d9d8dc677a655fb6691563310
zugzpark <70117647+zugzpark@users.noreply.github.com>
2023-11-28 15:46:48 +0900

    [Silver III] Title: 회전하는 큐, Time: 128 ms, Memory: 9364 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.js
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.js
@@ -1,5 +1,4 @@
-const line = require("fs").readFileSync("dev/stdin", "utf8");
-let input = line.trim().split("\n");
+const input = require('fs').readFileSync('dev/stdin').toString().trim().split('\n')
 
 let [N,M] = input[0].split(' ').map(v => +v)
 let arrM = input[1].split(' ').map(v => +v)
