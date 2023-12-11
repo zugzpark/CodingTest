897e24e11cfe0a4485ae4b6806cc1dcf86bcf006
Seong Jin Park <70117647+zugzpark@users.noreply.github.com>
2023-12-11 11:46:33 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,16 +1,20 @@
 function solution(progresses, speeds) {
-    let answer = [0];
-    let days = progresses.map((progress, index) => Math.ceil((100 - progress) / speeds[index]));
-    let maxDay = days[0];
+  const length = progresses.length;
+  let answer = [],
+    i = 0,
+    t = 0,
+    numberOfReleases = 0;
 
-    for(let i = 0, j = 0; i< days.length; i++){
-        if(days[i] <= maxDay) {
-            answer[j] += 1;
-        } else {
-            maxDay = days[i];
-            answer[++j] = 1;
-        }
+  while (i < length) {
+    numberOfReleases = 1;
+    t = Math.ceil((100 - progresses[i]) / speeds[i++]);
+
+    while (progresses[i] + speeds[i] * t > 99) {
+      numberOfReleases++;
+      i++;
     }
+    answer.push(numberOfReleases);
+  }
 
-    return answer;
+  return answer;
 }
