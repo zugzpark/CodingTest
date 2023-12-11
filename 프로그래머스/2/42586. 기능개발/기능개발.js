449a8e2f7f7bc1b42917d61df91245c188707964
Seong Jin Park <70117647+zugzpark@users.noreply.github.com>
2023-12-11 11:46:12 +0900

    [level 2] Title: 기능개발, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,106 +1,16 @@
 function solution(progresses, speeds) {
-    let temp = [];
-    let answer = []
-    let day = 0;
-    let boolean = false;
-    
-    class Node{
-       constructor(data){
-        this.data = data;
-        this.next = null;
-        this.prev = null;
-        
-       }
-    }
+    let answer = [0];
+    let days = progresses.map((progress, index) => Math.ceil((100 - progress) / speeds[index]));
+    let maxDay = days[0];
 
-    class Queue{
-        constructor(){
-            this.head = null;
-            this.tail = null;
-            this.size = 0;
-            this.index = 0;
-        }
-
-        push(data){
-            let node = new Node(data)
-
-            if(this.isEmpty()==0){
-                this.tail.next = node
-                node.prev = this.tail;
-            } else {
-                this.head = node;
-                this.head.next = null;
-            }
-            this.tail = node;
-            this.size++;
-        }
-
-        pop(){
-            if(this.isEmpty==1) reutrn -1;
-            
-            let popItem = this.head.data
-        
-            if(this.index>0 && boolean){    
-                
-                this.head.data += (speeds[this.index]*day)
-                boolean = false;
-            } else {
-                this.head.data += speeds[this.index]
-                    day++;
-            } 
-        
-            
-            if(this.head.data>=100){
-                this.index++
-                if(this.index>speeds.length)this.index=0;
-                temp.push(day)
-                this.head = this.head.next;
-                this.size--;
-                boolean = true;
-            } else {
-                boolean = false;
-            }
-            
-            return popItem
-        }
-
-        isEmpty(){
-            return this.size==0?1:0
-        }
-        
-        len(){
-            return this.size;
-        }
-        
-        printAll(){
-            let arr = [];
-            let cur = this.head
-            
-            while(cur){
-                arr.push(cur.data)
-                cur = cur.next
-            }
-            return arr
+    for(let i = 0, j = 0; i< days.length; i++){
+        if(days[i] <= maxDay) {
+            answer[j] += 1;
+        } else {
+            maxDay = days[i];
+            answer[++j] = 1;
         }
     }
 
-    let q = new Queue();
-    for(let i = 0 ; i<progresses.length ; i++){
-        q.push(progresses[i])
-    }
-    
-    while(q.isEmpty()!=1){
-        
-        q.pop()
-    }
-    
-    let result = temp.reduce((acc,cur) =>{
-        acc.set(cur, (acc.get(cur)||0) +1);
-        return acc
-    },new Map());
-    
-    for(let [key , value] of result.entries()){
-        answer.push(value)
-    }
     return answer;
 }
