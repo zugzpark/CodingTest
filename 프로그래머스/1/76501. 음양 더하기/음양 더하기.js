f45c29cfcb47b8c8f7ef43b55c3acf197a020c00
Seong Jin Park <70117647+zugzpark@users.noreply.github.com>
2023-12-15 10:13:41 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -0,0 +1,12 @@
+function solution(absolutes, signs) {
+    
+    let answer = 0;
+        
+    for(let i = 0 ; i<absolutes.length ; i++){
+        
+        signs[i]?answer+=absolutes[i]:answer+=(absolutes[i]*-1)
+        
+    }
+    
+    return answer;
+}
