1285d6e2d5578ac3fa5be07d1450f2ffb48aa346
Seong Jin Park <70117647+zugzpark@users.noreply.github.com>
2023-12-15 09:29:40 +0900

    [level 1] Title: 약수의 합, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    
+    let answer = 0;
+    
+    for(let i=0 ; i<=n ; i++){
+        if(n%i==0){
+            answer+=i;
+        }
+    }
+    
+    
+    return answer;
+}
