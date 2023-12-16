6c862083889909ff20fe7055ad7d3aa3c5db2694
Seong Jin Park <70117647+zugzpark@users.noreply.github.com>
2023-12-16 11:18:56 +0900

    [level 1] Title: 모의고사, Time: 4.21 ms, Memory: 37.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -0,0 +1,20 @@
+function solution(answers) {
+    let temp = [[1,2,3,4,5],[2,1,2,3,2,4,2,5],[3,3,1,1,2,2,4,4,5,5]];
+    
+    let num = [0,0,0]
+    let answer = []
+    
+    for(let i = 0 ; i<answers.length ; i++){
+        
+        if(answers[i]==temp[0][temp[0].length<=i?i%temp[0].length:i])num[0]++
+        if(answers[i]==temp[1][temp[1].length<=i?i%temp[1].length:i])num[1]++
+        if(answers[i]==temp[2][temp[2].length<=i?i%temp[2].length:i])num[2]++
+
+    }
+    
+    num.forEach((v,i) => {
+        if(Math.max(...num)==v)answer.push(i+1)
+    })
+    
+    return answer;
+}
