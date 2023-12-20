af0ce52f7bc0e7ee121c27971f2c1ee11f054d6e
Seong Jin Park <70117647+zugzpark@users.noreply.github.com>
2023-12-20 16:30:10 +0900

    [level 1] Title: 과일 장수, Time: 3.65 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -0,0 +1,26 @@
+function solution(k, m, score) {
+    var answer = 0;
+    let apple = []
+    let box = [];
+
+    // k = 최고점  , m 사과 포장 개수
+    // 
+    
+    score.sort((a,b) => a-b)
+    
+    console.log(score)
+    while(score.length>0){
+        
+        apple.push(score.pop())
+        if(apple.length==m){
+            box.push(apple)
+            apple = [];
+        }
+        
+    }
+    
+    box.forEach(v => {
+        answer+=Math.min(...v)*v.length
+    })
+    return answer;
+}
