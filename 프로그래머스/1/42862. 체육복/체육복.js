b7d208516e08b762ca20aa6412974cfb2d26e9fe
Seong Jin Park <70117647+zugzpark@users.noreply.github.com>
2024-01-09 09:53:09 +0900

    [level 1] Title: 체육복, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -0,0 +1,31 @@
+function solution(n, lost, reserve) {
+    var answer = 0;
+    let arr = new Array(n).fill(0)
+
+    
+    //체육복 잃어버린 아이 체크
+    for(let i = 0 ; i<lost.length ; i++){
+        arr[lost[i]-1]--
+    }
+    reserve.sort()
+    
+    //reserve중 앞뒤로 -1인것만 ++
+
+    for(let j = 0 ; j<reserve.length ; j++){
+    
+        if(arr[reserve[j]-1]==-1){
+            arr[reserve[j]-1]++
+            reserve[j]=null
+        }
+    }
+    
+    for(let j = 0 ; j<reserve.length ; j++){
+        if(arr[reserve[j]-1-1] && arr[reserve[j]-1-1]==-1 && !reserve[j]-1){
+            arr[reserve[j]-1-1]++;
+        } else if(arr[reserve[j]-1+1] && arr[reserve[j]-1+1]==-1 && !reserve[j]-1){
+            arr[reserve[j]-1+1]++;
+        }
+    }
+    
+    return arr.filter(v => v==0).length;
+}
