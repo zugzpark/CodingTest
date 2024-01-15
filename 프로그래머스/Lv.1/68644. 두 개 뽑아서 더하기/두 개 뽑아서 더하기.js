81318828e20981db7fdcdc6f948f4ca198506e3c
Seong Jin Park <70117647+zugzpark@users.noreply.github.com>
2024-01-15 09:40:43 +0900

    [level Lv.1] Title: 두 개 뽑아서 더하기, Time: 0.88 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/Lv.1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/Lv.1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -7,7 +7,7 @@ function solution(numbers) {
             answer.push(numbers[i]+numbers[j])
         }
     }
-    
+    // 배열안의 서로다른 인덱스 두개를 더 한 모든 값을 answer에 넣고 중복제거 후 정렬
     return [...new Set(answer)].sort((a,b) => a-b);
     
 }
