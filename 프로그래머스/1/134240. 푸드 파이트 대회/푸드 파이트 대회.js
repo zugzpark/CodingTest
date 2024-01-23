38c19107c05ced47cddc81505818f916007b087f
Seong Jin Park <70117647+zugzpark@users.noreply.github.com>
2024-01-23 09:40:48 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,7 +1,8 @@
 function solution(food) {
     let answer = [];
     let temp = []
-    
+    //음식의 종류과 개수가 같아야 하므로 짝수 갯수만큼 배열에 넣어준다
+    //단 짝수가 아닐경우는 패스
     for(let i = 1 ; i<food.length ; i++){
         
         let kind = food[i]
