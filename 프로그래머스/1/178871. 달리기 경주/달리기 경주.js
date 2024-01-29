eae865c285d6528d7a172a143c301eaeb68cff18
Seong Jin Park <70117647+zugzpark@users.noreply.github.com>
2024-01-29 09:38:44 +0900

    [level 1] Title: 달리기 경주, Time: 0.20 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -1,12 +1,11 @@
 function solution(players, callings) {
     let obj = {}
 
-    //순위 오브젝트로 담기
     for(let i = 0 ; i<players.length ; i++){
         obj[players[i]] = i
     }
 
-   //호출된 선수와 앞선수 순위 교체
+		
 	for(let j = 0; j<callings.length ; j++){
 		let idx = obj[callings[j]];
 		let temp = players[idx-1];
