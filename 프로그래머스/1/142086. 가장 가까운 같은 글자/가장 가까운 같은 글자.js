a9eec342ae59ee97af9c3bf5a07cdb9413ef6099
Seong Jin Park <70117647+zugzpark@users.noreply.github.com>
2024-02-07 14:28:30 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 2251.10 ms, Memory: 44.6 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,8 +1,12 @@
 function solution(s) {
     var answer = [];
-    for(let i = 0 ; i<s.length ; i++){
     
+    
+    for(let i = 0 ; i<s.length ; i++){
+        //문자열로 판별, 왼쪽부터 오른쪽이기 때문에 reverse 후 글자의 index를 찾는다
         let str = s.slice(0,i).split('').reverse().join('').indexOf(s[i]);
+      
+        //글자의 인덱스가 있으면 위치를 push, 없으면 -1 그대로 push
         if(s.slice(0,i).split('').reverse().join('').indexOf(s[i])>=0){
             answer.push(str+1)
         } else {
