7aebd04915e1986fdaad9af902a6c7802807cf18
Seong Jin Park <70117647+zugzpark@users.noreply.github.com>
2024-02-13 14:36:33 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -2,6 +2,7 @@ function solution(id_list, report, k) {
         
     let reportObj = new Map();
     
+    //신고, 신고당한횟수, 이용정지 내역 생성
     id_list.forEach(val => {
      (reportObj[val] = {
         report: [],
@@ -10,19 +11,23 @@ function solution(id_list, report, k) {
          });
     });
     
+    //정제
     report.forEach(val => {
     let [reportPerson, reportedPerson] = val.split(' ');
 
+    //신고한 사람이 목록에 없으면 +1
     if (reportObj[reportPerson].report.indexOf(reportedPerson) === -1) {
       reportObj[reportPerson].report.push(reportedPerson);
       reportObj[reportedPerson].reported += 1;
       
+      //신고 횟수가 k 이상이면 이용정지
       if (reportObj[reportedPerson].reported >= k) {
         reportObj[reportedPerson].isStoppedId = true;
       }
     }
   });
 
+  //id_list에 결과 메일리턴 
   let answer = id_list.map(val => {
   return reportObj[val].report.filter((reportId) => reportObj[reportId].isStoppedId).length;
   });
