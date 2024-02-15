286f1d449eb4968249e24feecca1b042ce88f216
Seong Jin Park <70117647+zugzpark@users.noreply.github.com>
2024-02-15 14:51:35 +0900

    [level 1] Title: 카드 뭉치, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,20 +1,14 @@
 function solution(cards1, cards2, goal) {
-    
-    for(let i = 0 ; i<goal.length ; i++){
-        
-        if(goal[i]==cards1[0]){
+    for (let i = 0; i < goal.length; i++) {
+        if (goal[i] === cards1[0]) {
             cards1.shift();
-            goal.shift();
-            i--;
-        } else if(goal[i]==cards2[0]){
+        } else if (goal[i] === cards2[0]) {
             cards2.shift();
-            goal.shift();
-            i--;
         } else {
-            return "No"
+            return "No";
         }
-        
+        goal.shift();
+        i--;
     }
-
     return "Yes";
 }
