d511cd89700993203171d0afb20a2d59406d679b
Seong Jin Park <70117647+zugzpark@users.noreply.github.com>
2024-02-16 14:39:14 +0900

    [level 1] Title: 기사단원의 무기, Time: 2.37 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,15 +1,17 @@
 function solution(number, limit, power) {
     var answer = 0;
     let temp = []
-
+    
+    //약수의 개수 혹은 제한된 power리턴
     for(let i = 1 ; i<=number ; i++){
         temp.push(measure(i,limit,power))
     }
     
+    //reduce로 철 무게 리턴
     answer = temp.reduce((acc,val) => acc+=val)
     return answer;
 }
-
+// 약수의 개수와 제한 비교 공격력이 limit을 넘으면 power리턴
 function measure(num,limit,power){
     let cnt = new Set()
     for(let i = 1; i<=Math.sqrt(num) ; i++){
