d84f007cce52084fccbbe1b834f8bc14114eaf8e
Seong Jin Park <70117647+zugzpark@users.noreply.github.com>
2024-02-22 14:58:53 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.41 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -1,11 +1,11 @@
 function solution(strings, n) {
     var answer = [];
     let word = [];
-    
+    //첫글자 배열에 담기
     strings.forEach(v => {
         word.push([v.slice(n,n+1),v])
     })
-    
+    //첫글자 기준으로 정렬, 같으면 사전순 정렬
     word.sort((a,b) => {
     
         if(a[0]===b[0])return a[1]>b[1]?1:-1
