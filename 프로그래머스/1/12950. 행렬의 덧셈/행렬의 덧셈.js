f92dc399aaa9a707ee55d5e06f977e9f74f90053
Seong Jin Park <70117647+zugzpark@users.noreply.github.com>
2024-02-28 14:48:23 +0900

    [level 1] Title: 행렬의 덧셈, Time: 9.05 ms, Memory: 67.5 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -4,7 +4,7 @@ function solution(arr1, arr2) {
     for(let i=0; i<arr1.length ; i++){ 
         let sum = [];
         for(let j=0; j<arr1[i].length ; j++){
-        
+        //고정값으로 처리
         sum.push(arr1[i][j]+arr2[i][j])
             
         }
